735d2f0852c239e72cae05935314d57eb812ee53
Ricky Hanlon <rickhanlonii@gmail.com>
2024-02-16 11:56:48 -0500

    updates

--- canary/src/index.js
+++ canary/src/index.js
@@ -6,3 +6,15 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(<App />);
+
+window.addEventListener("message", ({ data }) => {
+  if (typeof data === "string" && data[0] === "{") {
+    console.log("data", data);
+    const messageData = JSON.parse(data);
+    if (messageData.type === "throttle") {
+      window.FALLBACK_THROTTLE_MS = parseInt(messageData.value, 10);
+    } else if (messageData.type === "revert") {
+      window.REVERT_SIBLING_RENDER = !!messageData.value;
+    }
+  }
+});
--- canary/src/other-src/index.js
+++ canary/src/other-src/index.js
@@ -1,8 +1,20 @@
-import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(<App />);
+
+window.addEventListener("message", ({ data }) => {
+  if (typeof data === "string" && data[0] === "{") {
+    console.log("data", data);
+    const messageData = JSON.parse(data);
+    if (messageData.type === "throttle") {
+      window.FALLBACK_THROTTLE_MS = parseInt(messageData.value, 10);
+    } else if (messageData.type === "revert") {
+      window.REVERT_SIBLING_RENDER = !!messageData.value;
+    }
+  }
+});
